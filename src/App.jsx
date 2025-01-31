58b18b47e8bce3cdd3b99dc67f1c4d64fc9ecc9f
swa1245 <swarajpatil2123@gmail.com>
2025-01-31 23:43:26 +0530

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
 
+import '@solana/wallet-adapter-react-ui/styles.css';
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+          <WalletProvider wallets={[]} autoConnect>
+              <WalletModalProvider>
+                <div style={{ display: 'flex', justifyContent: "space-between" }}>
+                  <WalletMultiButton />
+                  <WalletDisconnectButton />
+                </div>
+                {/* <RequestAirdrop />
+                <ShowSolBalance /> */}
+                {/* <Tokens /> */}
+                {/* <SignMessage />
+                <SendTokens /> */}
+              </WalletModalProvider>
+          </WalletProvider>
+      </ConnectionProvider>
   )
 }
 
